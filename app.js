8ad33798b7bf63c37bf87ceddfe58f60ff497ea5
Elimax Mwangi <elimax@localhost.localdomain>
2019-09-20 15:18:10 -0400

    use of express, chalk, debug and morgan

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
+
+
+var app = express();
+app.use(morgan('tiny'));
+app.get('/', function(req, res){
+    res.send('hello from librarian');
+
+})
+app.listen(3000, function(){
+debug(`listening on port ${chalk.green('3000')}`);
+});
