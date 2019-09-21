2c6153b1a25647f2364f65ab341d41d4bec3c3a7
Elimax Mwangi <elimax@localhost.localdomain>
2019-09-21 15:07:24 -0400

    static files-public directoty

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var path =  require('path');
 
 var app = express();
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname,  '/public/')));
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname,'/views/index.html'));
 
